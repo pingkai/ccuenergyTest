016e72c12108b5e772908b83e8f91c83f89a3bec
pingkai <pingkai010@gmail.com>
2023-07-23 10:38:11 -0400

    style(echoserver): let EchoServer under the namespace AccuEnergyTest
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- src/EchoServer.h
+++ src/EchoServer.h
@@ -11,60 +11,61 @@
 #include <memory>
 #include <thread>
 #include <unordered_map>
-using namespace AccuEnergyTest;
-class EchoServer : private IServer::Listener {
-
-public:
-    class EchoTask {
-    public:
-        EchoTask(std::unique_ptr<SimpleMemPool::PoolMemory> &&buffer, int size) : mBufferSize(size), mBuffer(std::move(buffer))
-        {}
+namespace AccuEnergyTest {
+    class EchoServer : private IServer::Listener {
 
     public:
-        uint8_t *getBuffer()
-        {
-            return mBuffer->get() + index;
-        }
+        class EchoTask {
+        public:
+            EchoTask(std::unique_ptr<SimpleMemPool::PoolMemory> &&buffer, int size) : mBufferSize(size), mBuffer(std::move(buffer))
+            {}
 
-        void skip(int p)
-        {
-            index += p;
-            mBufferSize -= p;
-        }
-        int getBufferSize()
-        {
-            return mBufferSize;
-        }
+        public:
+            uint8_t *getBuffer()
+            {
+                return mBuffer->get() + index;
+            }
 
-    private:
-        int mBufferSize;
-        std::unique_ptr<SimpleMemPool::PoolMemory> mBuffer;
-        int index{};
-    };
-    explicit EchoServer(IServer &server);
+            void skip(int p)
+            {
+                index += p;
+                mBufferSize -= p;
+            }
+            int getBufferSize()
+            {
+                return mBufferSize;
+            }
 
-    int start();
+        private:
+            int mBufferSize;
+            std::unique_ptr<SimpleMemPool::PoolMemory> mBuffer;
+            int index{};
+        };
+        explicit EchoServer(IServer &server);
+
+        int start();
 #ifdef NDEBUG
 #else
-    int startForTest();
+        int startForTest();
 #endif
 
-    ~EchoServer() override;
+        ~EchoServer() override;
 
-private:
-    int onClientRead(const IServer &server, int64_t id) override;
-    int onClientWrite(const IServer &server, int64_t id) override;
-    int init();
-    void serverLoop();
-    int loopOnce();
-    int echoBack(int64_t id);
+    private:
+        int onClientRead(const IServer &server, int64_t id) override;
+        int onClientWrite(const IServer &server, int64_t id) override;
+        int init();
+        void serverLoop();
+        int loopOnce();
+        int echoBack(int64_t id);
 
-private:
-    IServer &mServer;
-    std::unique_ptr<std::thread> mThread{};
-    std::atomic<bool> mStop{};
-    std::unordered_map<int64_t, std::unique_ptr<EchoTask>> mTaskMap{};
-};
+    private:
+        IServer &mServer;
+        std::unique_ptr<std::thread> mThread{};
+        std::atomic<bool> mStop{};
+        std::unordered_map<int64_t, std::unique_ptr<EchoTask>> mTaskMap{};
+    };
+}// namespace AccuEnergyTest
 
 
 #endif//ACCUENERGYTEST_ECHOSERVER_H
